ddab910b9959815a1a1ada437fee305412209e9a
tiagofribeiro <tg.frnds@outlook.com>
2025-02-11 23:08:50 -0300

    :gear: base structure

---
+++ README.md
@@ -0,0 +1,19 @@
+![logo](/src/logo.svg)
+
+<br>
+
+## TGFR-UI
+
+Biblioteca de componentes usados em diferentes projetos.
+
+<br>
+
+### Licença
+
+Este projeto é licenciado sob a [MIT License](LICENSE).
+
+<br>
+
+### Créditos
+
+Esta biblioteca usa o [styled-components](https://styled-components.com).
---
+++ src/components/Text/index.tsx
@@ -0,0 +1,41 @@
+import { Body, Header } from "./style";
+import { TextTypes } from "./types";
+
+const Text = ({
+  title,
+  as,
+  bold,
+  primary,
+  maxW,
+  marginX,
+  marginY,
+  alignment,
+  fontSize,
+  color,
+  children
+}: TextTypes) => {
+  return title ?
+    <Header
+      as={as}
+      $maxW={maxW}
+      $marginX={marginX}
+      $marginY={marginY}
+      $alignment={alignment}
+      $color={color}>
+      {children}
+    </Header>
+    :
+    <Body
+      $bold={bold}
+      $primary={primary}
+      $maxW={maxW}
+      $marginX={marginX}
+      $marginY={marginY}
+      $alignment={alignment}
+      $fontSize={fontSize}
+      $color={color}>
+      {children}
+    </Body>
+};
+
+export default Text;
